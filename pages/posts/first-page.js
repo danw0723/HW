9df3ce1326abe713495e1217e5a41c09be6be459
danw0723 <danw723@naver.com>
2023-04-10 13:55:55 +0900

    First Commit

---
+++ pages/posts/first-page.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default function FirstPage() {
+    return (
+        <div>
+            <h2>My name is SangWoo Woo(Made in Korea)</h2> 
+            <Link href="/">Back to home</Link>
+        </div>
+    )
+}
