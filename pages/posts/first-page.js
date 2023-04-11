d63c139812f4cad799ccf8a42b62b95db76e520a
danw0723 <danw723@naver.com>
2023-04-11 13:44:16 +0900

    First Commit

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -2,7 +2,13 @@ import Link from 'next/link'
 export default function FirstPage() {
     return (
         <div>
-            <h2>My name is SangWoo Woo(Made in Korea)</h2> 
+            <h1>SangWoo Woo</h1>
+            <h3>Birth date : 2003. 07. 23</h3>
+            <h3>MBTI : ISTJ</h3>
+            <h3>Instagram ID : @_wsw.03</h3>
+            <h2>So do not fear, for I am with you; I will uphold you with my righteous right hand.</h2>
+            <h4>(Isaiah 41:10)</h4>
+            <img src="https://imgur.com/CWp0xjw.gif" alt="My photo" />
             <Link href="/">Back to home</Link>
         </div>
     )
